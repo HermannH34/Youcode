e870e663a00386767eb35e6ce006f41ef5ccce7a
Hermann Hairet <hermannhairet44@gmail.com>
2023-12-21 20:35:11 +0100

    add Admin page

--- src/components/AdminPage.tsx
+++ src/components/AdminPage.tsx
@@ -12,22 +12,51 @@ import {
 } from "@/components/ui/card";
 
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
 
 
 export const AccountPage = ({ courses }) => {
     const { data: session } = useSession();
 
-    console.log('ICI courses ', courses);
-
     if (session) {
         return (
-            <Card>
-                {courses.map((course) =>
-                    course.userId === session.user.id ? (
-                        <CardTitle key={course.id}>{course.name}</CardTitle>
-                    ) : null
-                )}
-            </Card>
+            <div>
+                <h1 className='pl-8 text-3xl'>Courses</h1>
+                <Card className="p-4 w-[1000px] h-[300px] m-4">
+                    <Table>
+                        <TableHeader >
+                            <TableRow>
+                                <TableHead className="w-50">Image</TableHead>
+                                <TableHead className="w-30">Name</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {courses.filter(course => course.userId === session.user.id).map((course) => (
+                                <TableRow key={course.id} >
+                                    <TableCell className="font-medium">
+                                        <Avatar>
+                                            <AvatarImage src={course.image} />
+                                        </Avatar>
+                                    </TableCell>
+                                    <TableCell>
+                                        <CardTitle>{course.name}</CardTitle>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Card>
+            </div>
         );
     }
 
